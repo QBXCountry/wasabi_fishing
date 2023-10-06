f6964fa60b38b11c3baa4614ee441f9464850985
CarlBishop <peppino@galad.it>
2023-10-06 13:31:23 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -10,10 +10,20 @@ version '2.0.7'
 author 'wasabirobby'
 description 'Wasabi ESX/QBCore Skill Based Fishing'
 
-shared_scripts { '@ox_lib/init.lua', 'configuration/*.lua' }
+shared_scripts { 
+    '@ox_lib/init.lua',
+    '@qbx_core/import.lua',
+    '@qbx_core/shared/locale.lua',
+    'configuration/*.lua' 
+}
 
 client_scripts { 'bridge/**/client.lua', 'client/*.lua' }
 
 server_scripts { 'bridge/**/server.lua', 'server/*.lua' }
+
+modules {
+    'qbx_core:playerdata',
+    'qbx_core:utils',
+}
 
 dependencies { 'ox_lib' }
